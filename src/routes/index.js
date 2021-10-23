c872e7d1bcb38474d79d06bfa40ea02f1acdcefa
KingGnome115 <kevinjec115@gmail.com>
2021-10-23 13:14:04 -0500

    Ya se eliminan los datos de la base de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,20 @@ const Task = require('../models/task')
 
 router.get('/', async (req, res)=>{
     const tasks = await Task.find();
-    console.log(tasks)
     res.render('index',{
         tasks
     })
 });
 
-router.post('/add', async (req, res) =>{
+router.post('/add', async (req, res)=>{
     const task = new Task(req.body);
     await task.save();
+    res.redirect('/')
+});
+
+router.get('/delete/:id', async (req,res)=>{
+    const {id} = req.params;
+    await Task.remove({_id:id});
     res.redirect('/')
 });
 
