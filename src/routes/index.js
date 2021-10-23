0662fcad17b91f3b908dd8b61dac1762714c49b2
KingGnome115 <kevinjec115@gmail.com>
2021-10-23 18:21:54 -0500

    La bd ya actualiza los datos en mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,6 +24,20 @@ router.get('/turn/:id', async (req, res)=>{
     res.redirect('/');
 });
 
+router.get('/edit/:id', async (req, res)=>{
+    const {id} = req.params;
+    const task = await Task.findById(id);
+    res.render('edit',{
+        task
+    });
+});
+
+router.post('/edit/:id', async (req, res)=>{
+    const {id} = req.params;
+    await Task.update({_id: id}, req.body);
+    res.redirect('/');
+});
+
 router.get('/delete/:id', async (req,res)=>{
     const {id} = req.params;
     await Task.remove({_id:id});
