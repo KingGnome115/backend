e10d769ff518fc49c0a2ee6372e91c2154399643
KingGnome115 <kevinjec115@gmail.com>
2021-10-23 01:38:40 -0500

    Ya hace altas en mongoDB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task')
+
 router.get('/', (req, res)=>{
     res.render('index')
-})
+});
+
+router.post('/add', async (req, res) =>{
+    
+    const task = new Task(req.body);
+    await task.save();
+    res.send('recibido');
+});
 
 module.exports = router;
