73f0b5f4cdd7566bd2a05a9818c9989010d7ac1b
KingGnome115 <kevinjec115@gmail.com>
2021-10-23 02:15:38 -0500

    Ya se representan datos en la tabla principal

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,18 @@ const router = express.Router();
 
 const Task = require('../models/task')
 
-router.get('/', (req, res)=>{
-    res.render('index')
+router.get('/', async (req, res)=>{
+    const tasks = await Task.find();
+    console.log(tasks)
+    res.render('index',{
+        tasks
+    })
 });
 
 router.post('/add', async (req, res) =>{
-    
     const task = new Task(req.body);
     await task.save();
-    res.send('recibido');
+    res.redirect('/')
 });
 
 module.exports = router;
